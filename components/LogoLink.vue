09e2d865e26dcb7330ef1f0edca50bb10bace583
joaotolovi <joao.tolovi@keeps.com.br>
2024-05-30 21:14:01 -0300

    init

---
+++ components/LogoLink.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+  url: String,
+  useNextImage: Boolean,
+});
+
+const logoSrc = ref(props.useNextImage? '/_next/image?url=%2Fassets%2Fimages%2Flogo.png&w=48&q=75' : '/assets/images/logo.png');
+const logoWordsSrc = ref('/assets/images/logo-words.svg');
+</script>
